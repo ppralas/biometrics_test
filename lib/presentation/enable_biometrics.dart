9348543cdf22e06592abb32d4ea13e96691ab7e4
Patrik Pralas <patrik.pralas@q.agency>
2023-06-23 16:16:51 +0200

    first commit

---
+++ lib/presentation/enable_biometrics.dart
@@ -0,0 +1,92 @@
+import 'package:biometric/presentation/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:local_auth/local_auth.dart';
+
+class BiometricsPage extends StatefulWidget {
+  @override
+  _BiometricsPageState createState() => _BiometricsPageState();
+}
+
+class _BiometricsPageState extends State<BiometricsPage> {
+  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
+  final LocalAuthentication _localAuth = LocalAuthentication();
+
+  bool _biometricsEnabled = false;
+
+  Future<void> _toggleBiometrics(bool value) async {
+    if (value) {
+      bool canCheckBiometrics = await _localAuth.canCheckBiometrics;
+      if (canCheckBiometrics) {
+        setState(() {
+          _biometricsEnabled = true;
+        });
+      } else {
+        setState(() {
+          _biometricsEnabled = false;
+        });
+        // Show error or display a message that the device does not support biometrics.
+      }
+    } else {
+      setState(() {
+        _biometricsEnabled = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadBiometricsStatus();
+  }
+
+  Future<void> _loadBiometricsStatus() async {
+    String? enabled = await _secureStorage.read(key: 'biometricsEnabled');
+    setState(() {
+      _biometricsEnabled = enabled == 'true';
+    });
+  }
+
+  Future<void> _saveBiometricsStatus(bool value) async {
+    await _secureStorage.write(
+        key: 'biometricsEnabled', value: value.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Text(
+              'Enable Biometrics',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            const SizedBox(height: 16.0),
+            Column(
+              children: [
+                Switch(
+                  value: _biometricsEnabled,
+                  onChanged: (value) {
+                    _toggleBiometrics(value);
+                    _saveBiometricsStatus(value);
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () => Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginPage(),
+                    ),
+                  ),
+                  child: const Text('Log out'),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/login_page.dart
@@ -0,0 +1,140 @@
+import 'package:biometric/presentation/enable_biometrics.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:local_auth/local_auth.dart';
+
+void main() {
+  runApp(const Login());
+}
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
+  final LocalAuthentication _localAuth = LocalAuthentication();
+  final List<String> _validEmails = [
+    'user1@example.com',
+    'user2@example.com',
+    'user3@example.com',
+  ];
+  final List<String> _validPasswords = [
+    'password1',
+    'password2',
+    'password3',
+  ];
+
+  String _errorMessage = '';
+
+  Future<bool> _authenticate() async {
+    try {
+      bool canCheckBiometrics = await _localAuth.canCheckBiometrics;
+
+      if (canCheckBiometrics) {
+        List<BiometricType> availableBiometrics =
+            await _localAuth.getAvailableBiometrics();
+        if (availableBiometrics.isNotEmpty) {
+          return await _localAuth.authenticate(
+            localizedReason: 'Authenticate to access the app',
+          );
+        }
+      }
+    } catch (e) {
+      print('Authentication error: $e');
+    }
+    return false;
+  }
+
+  void _login() async {
+    final enteredEmail = _emailController.text.trim();
+    final enteredPassword = _passwordController.text.trim();
+
+    if (_validEmails.contains(enteredEmail) &&
+        _validPasswords.contains(enteredPassword)) {
+      String? canUseBiometrics =
+          await _secureStorage.read(key: 'biometricsEnabled');
+      bool authenticated = true;
+      if (canUseBiometrics == 'true') {
+        authenticated = await _authenticate();
+      }
+
+      if (context.mounted && authenticated) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => BiometricsPage(),
+          ),
+        );
+      }
+    } else {
+      setState(() {
+        _errorMessage = 'Wrong credentials. Please try again.';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _login,
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 16.0),
+            if (_errorMessage.isNotEmpty)
+              Text(
+                _errorMessage,
+                style: const TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
